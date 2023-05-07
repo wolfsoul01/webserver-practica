940a856fa6fd7cfc43fce0eb3e5202180e2fd61b
wolfsoul <alejandrof011021@gmail.com>
2023-05-07 18:12:19 -0400

    Levantando server con http

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+import http from 'http';
 
 
-console.log('Hola mundo ');w
+const server = http.createServer((req,res)=>{
+
+    res.writeHead(200,{'Content-type':'text/plain'});
+
+    res.write('Hola mundo desde el server ');
+    res.end()
+})
+
+const PORT= 8080;
+server.listen(PORT);
+console.log('Server listo en el localhost://8080');
+
+
