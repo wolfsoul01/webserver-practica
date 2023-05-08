0a3fa0712f8f879af9bf50af666675522d465493
wolfsoul <alejandrof011021@gmail.com>
2023-05-08 16:16:25 -0400

    Hola mundo desde express

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+import express from 'express';
 
+const app = express();
+const PORT=8080;
+app.get('/',(req,res)=>{
 
-console.log("Hola mundo ");
+    res.send('Hola mundo desde express')
+})
+
+app.listen(PORT,()=>{
+    console.log("Server listo en el localhost:",PORT);
+})
