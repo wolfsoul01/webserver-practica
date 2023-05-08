ede780b0b8d2f9133883acb9e35fb6a6c8959fe7
wolfsoul <alejandrof011021@gmail.com>
2023-05-08 16:31:15 -0400

    Cambios

--- main.js
+++ main.js
@@ -4,8 +4,17 @@ const app = express();
 const PORT=8080;
 app.get('/',(req,res)=>{
 
-    res.send('Hola mundo desde express')
+    res.send('Hola mundo desde express');
+});
+
+app.get('/api',(req,res)=>{
+    res.send('Aqui va la api perro ');
 })
+
+app.get('*',(req,res)=>{
+    res.send('404 | Page not Found ;')
+})
+
 
 app.listen(PORT,()=>{
     console.log("Server listo en el localhost:",PORT);
