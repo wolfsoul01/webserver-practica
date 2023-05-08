7370e099f328c7b5a05d428cee9d7285121602c8
wolfsoul <alejandrof011021@gmail.com>
2023-05-08 16:09:46 -0400

    Se instalo express para el nuevo server

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-import http from 'http';
-import fs from 'fs'
-
-const html=fs.readFileSync('./index.html',{encoding:"utf8"})
-
-const server = http.createServer((req,res)=>{
-
-    res.writeHead(200,{'Content-type':'html'});
-
-    res.write(html);
-    res.end()
-})
-
-const PORT= 8080;
-server.listen(PORT);
-console.log('Server listo en el http://localhost:8080/');
 
 
+console.log("Hola mundo ");
