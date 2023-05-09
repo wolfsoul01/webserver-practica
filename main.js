33eafcc4275f4a43db19f3a3b5da98f0d0c38025
wolfsoul <alejandrof011021@gmail.com>
2023-05-09 10:43:33 -0400

    Cambio en la estructura uso de handlebars para el prerenderizado y no recuerdo que mas :)

--- main.js
+++ main.js
@@ -2,18 +2,30 @@ const express = require('express');
 const app = express();
 const PORT=8080;
 
+app.set('view engine', 'hbs');
+
 app.use(express.static('public'));
 
 
-app.get('/api',(req,res)=>{
-    res.send('Aqui va la api perro ');
-})
+app.get('/',(req,res)=>{
+    res.render('home',{
+        nombre:'WolfSoul',
+        titulo:'Curso Node '
+    })
+});
+app.get('/elements',(req,res)=>{
+    res.sendFile(__dirname+'/public/elements.html');
+});
+
+app.get('/generic',(req,res)=>{
+    res.sendFile(__dirname+'/public/generic.html');
+});
 
 app.get('*',(req,res)=>{
     res.sendFile(  __dirname+'/public/404.html');
-})
+});
 
 
 app.listen(PORT,()=>{
     console.log("Server listo en el localhost:",PORT);
-})
+});
